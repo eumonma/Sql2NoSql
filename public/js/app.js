48af9c50072db5b4ce94a82851507df27385e536
EMM Cap <eugenio.montoro@gmail.com>
2021-03-15 19:54:00 +0100

    Capitulo 3 del video tutorial de Firebase Functions

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+const requestModal = document.querySelector('.new-request'); // Hace referencia al documento modal a abrir
+const requestLink = document.querySelector('.add-request'); // HAce referencia al elemento sobre el que se hace Click para abrir la Modal
+
+// open request modal
+requestLink.addEventListener('click', () => {
+  requestModal.classList.add('open');
+});
+
+// para poder cerrar el modal
+requestModal.addEventListener('click', (e) => {
+    if(e.target.classList.contains('new-request')){
+        requestModal.classList.remove('open');
+    }
+})
